e04cc23ae9803f10ab3c4acb996ad24e3a8ec6b0
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2024-01-15 18:23:25 +0100

    use axios
    and trigger deploy with subdomain

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,35 +3,29 @@ import { jwtDecode } from 'jwt-decode';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-/*
 const login = async (username, password) => {
-  const response = await axios.post(`${API_URL}admin/log`, {
-    username,
-    password,
-  });
-  if (response.data.token) {
-    localStorage.setItem('userToken', response.data.token);
-    notificationService.notify('Connecté(e) !', 'success');
+  try {
+    const response = await axios.post(
+      `${API_URL}admin/log`,
+      {
+        username,
+        password,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+
+    if (response.data.token) {
+      localStorage.setItem('userToken', response.data.token);
+      notificationService.notify('Connecté(e) !', 'success');
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la requête de connexion :', error);
+    throw error;
   }
-  return response.data;
-};
-*/
-
-const login = async (username, password) => {
-  const data = {
-    username: username,
-    password: password,
-  };
-
-  const response = await fetch(`${API_URL}admin/log`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  if (!response.ok) throw new Error('Erreur, vérifiez vos identifiants');
-  return await response.json();
 };
 
 const logout = () => {
